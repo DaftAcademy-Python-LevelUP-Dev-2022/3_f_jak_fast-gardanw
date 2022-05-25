6f070a59fc4f7b1350762bb3fb1dbf41a3f83f1e
Damian <gardanw@gmail.com>
2022-05-25 21:15:05 +0200

    start endpoint

--- homework3.py
+++ homework3.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+
+@app.get('/start', response_class=HTMLResponse)
+def start():
+    return '<h1>The unix epoch started at 1970-01-01</h1>'
